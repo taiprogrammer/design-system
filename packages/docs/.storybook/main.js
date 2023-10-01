2c3f1ce4af1ea82a09d277c76cb1a2218fd81109
taiprogrammer <taiza.marques@sptech.school>
2023-10-01 19:52:25 -0300

    why storybook isnt running?

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,31 +1,49 @@
-import { join, dirname } from "path";
+// import { join, dirname } from "path";
 
-/**
- * This function is used to resolve the absolute path of a package.
- * It is needed in projects that use Yarn PnP or are set up within a monorepo.
- */
-function getAbsolutePath(value) {
-  return dirname(require.resolve(join(value, "package.json")));
+// /**
+//  * This function is used to resolve the absolute path of a package.
+//  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+//  */
+// function getAbsolutePath(value) {
+//   return dirname(require.resolve(join(value, "package.json")));
+// }
+
+// /** @type { import('@storybook/react-vite').StorybookConfig } */
+// const config = {
+//   stories: [
+//     "../stories/**/*.mdx",
+//     "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+//   ],
+//   addons: [
+//     getAbsolutePath("@storybook/addon-links"),
+//     getAbsolutePath("@storybook/addon-essentials"),
+//     getAbsolutePath("@storybook/addon-onboarding"),
+//     getAbsolutePath("@storybook/addon-interactions"),
+//   ],
+//   framework: {
+//     name: getAbsolutePath("@storybook/react-vite"),
+//     options: {},
+//   },
+//   docs: {
+//     autodocs: "tag",
+//   },
+// };
+// export default config;
+module.exports = {
+  'stories': [
+    '../src/pages/**/*.storie.mdx',
+    '../src/stories/**/*.storie.tsx'
+  ],
+  'addons': [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions'
+  ],
+  'framework': '@storybook/react',
+  'core': {
+    'builder': '@storybook/builder-vite'
+  },
+  'features': {
+    'storyStoreV7': true
+  }
 }
-
-/** @type { import('@storybook/react-vite').StorybookConfig } */
-const config = {
-  stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-  ],
-  addons: [
-    getAbsolutePath("@storybook/addon-links"),
-    getAbsolutePath("@storybook/addon-essentials"),
-    getAbsolutePath("@storybook/addon-onboarding"),
-    getAbsolutePath("@storybook/addon-interactions"),
-  ],
-  framework: {
-    name: getAbsolutePath("@storybook/react-vite"),
-    options: {},
-  },
-  docs: {
-    autodocs: "tag",
-  },
-};
-export default config;
--- packages/docs/storybook-static/assets/syntaxhighlighter-4OVWVELQ-f324372a.js
+++
@@ -1,2 +0,0 @@
-import{S as l,c as n,s as d}from"./index-9b44ae56.js";import"./iframe-4784f9dc.js";import"../sb-preview/runtime.js";import"./index-d475d2ea.js";import"./index-61bf1805.js";import"./_commonjsHelpers-de833af9.js";import"./index-6a5bd4ef.js";import"./index-d37d4223.js";import"./index-356e4a49.js";export{l as SyntaxHighlighter,n as createCopyToClipboardFunction,d as default};
-//# sourceMappingURL=syntaxhighlighter-4OVWVELQ-f324372a.js.map
